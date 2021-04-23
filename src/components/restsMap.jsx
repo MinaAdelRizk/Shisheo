adf9de475dcd459d7c38adb4c6c9b5a930f9190b
Mina Adel <mina.adel121@gmail.com>
2021-04-23 23:10:04 +0400

    Adding search the search to display InfoWindow for results on match

--- src/components/restsMap.jsx
+++ src/components/restsMap.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import SearchBox from '../common/searchBox';
 import {
     GoogleMap,
@@ -7,28 +7,72 @@ import {
     Marker,
     InfoWindow
 } from 'react-google-maps'
+
 import { getRestaurants } from './../services/fakeRestServices';
+
+// const {
+//     SearchBox
+// } = require("react-google-maps/lib/components/places/StandaloneSearchBox");
+
+
+// const Box = () => {
+//     return (
+//         <SearchBox
+//             ref={props.onSearchBoxMounted}
+//             bounds={props.bounds}
+//             onPlacesChanged={props.onPlacesChanged}
+//         >
+//             <input
+//                 type="text"
+//                 placeholder="Search restaurants..."
+//                 style={{
+//                     boxSizing: `border-box`,
+//                     border: `1px solid transparent`,
+//                     width: `80vw`,
+//                     height: `3rem`,
+//                     padding: `0 12px`,
+//                     borderRadius: `3px`,
+//                     boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+//                     fontSize: `14px`,
+//                     outline: `none`,
+//                     textOverflow: `ellipses`
+//                 }}
+//             />
+//         </SearchBox>
+//     )
+// }
 
 
 function Map() {
 
-    const [selectedRst, setSelectedRst] = useState();
-    const locations = getRestaurants()
+    let [zoom, setZoom] = useState(10) // Map Zoom
+    let [pos, setPos] = useState({ lat: 25.117676, lng: 55.201827 }) // Map Center
+    let [selectedRst, setSelectedRst] = useState();
+    let [searchQuery, setSearchQuery] = useState("");
+    let locations = getRestaurants()
+
+    useEffect(() => {
+        locations.length === 1 ? setSelectedRst(locations[0]) : setSelectedRst(null);
+    }, [searchQuery]);
+
+    locations = (searchQuery !== "") ? locations.filter(r => r.name.toLowerCase().includes(searchQuery.toLowerCase())) : locations;
 
     return (
+
+
         <GoogleMap
-            zoom={10}
-            center={{
-                lat: 25.117676, lng: 55.201827
-            }}
+            zoom={zoom}
+            center={pos}
         >
+            <SearchBox
+                value={searchQuery}
+                onChange={setSearchQuery}
+            />
+
             {locations.map(r => (
                 <Marker
                     key={r._id}
-                    position={{
-                        lat: r.location.lat,
-                        lng: r.location.lng
-                    }}
+                    position={{ lat: r.location.lat, lng: r.location.lng }}
                     icon={{
                         url: '/location.svg',
                         scaledSize: new window.google.maps.Size(40, 30)
